ac59ed5f5c9975c4950204e129e8d0219e75736c
ciamac-da <ciamac.davoudi@gmail.com>
2021-07-11 13:18:02 +0200

    Verified by Marti4n v1.0.5 js done

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)")
 const count = document.getElementById("count")
 const total = document.getElementById("total")
 const movieSelect = document.getElementById("movie")
-const ticketPrice = +movieSelect.value
+let ticketPrice = +movieSelect.value
 
+
+// Update total and count
+const updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected")
+  const selectedSeatsCount = selectedSeats.length
+  count.innerText = selectedSeatsCount
+  total.innerText = selectedSeatsCount * ticketPrice
+}
+
+
+// Movie select event
+movieSelect.addEventListener("change", (e) => {
+ticketPrice = +e.target.value
+updateSelectedCount()
+})
+
+
+// Seat click event
 container.addEventListener("click", (e) => {
 if(
   e.target.classList.contains("seat") &&
   !e.target.classList.contains("occupied")
 ) {
   e.target.classList.toggle("selected")
+  updateSelectedCount()
 }
 })
